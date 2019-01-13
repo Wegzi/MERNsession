ea42b197146766ab51799c367f8668df1e6feacc
Lucas Ramos <wegzi.ramos@gmail.com>
2019-01-13 20:08:37 -0200

    mongo config

---
+++ config/config.exemple.js
@@ -0,0 +1,6 @@
+// Copy this file as config.js in the same folder, with the proper database connection URI.
+
+module.exports = {
+  db: 'mongodb://usuario:senha@url:27017/db',
+  db_dev: 'mongodb://localhost:27017/db',
+};
